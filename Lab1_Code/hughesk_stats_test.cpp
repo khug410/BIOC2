731d726f0b2180ec0519fd1fa62538bb3446f87b
khug410 <kelsey.hughes@marquette.edu>
2022-02-02 16:53:22 -0600

    added logic for getMin() and getMax() functions

--- Lab1_Code/hughesk_stats_test.cpp
+++ Lab1_Code/hughesk_stats_test.cpp
@@ -6,3 +6,41 @@ Program: This header file will define functions that estimate statistics about a
 
 #include "hughesk_stats.hpp"
 
+#include <iostream>
+#include <math.h>
+#include <unistd.h>
+#include <fstream>
+
+//instantiate the necessary variables from the header file
+void BigKels::Stattys::stats(float min, float max, float mean, float STD){
+    this -> min = min;
+    this -> max = max;
+    this -> mean = mean;
+    this -> STD = STD;
+}
+
+//call and calculate the getMin() function
+float BigKels::Stattys::getMin(std::vector<float>* data, int n){
+    int i = 0;
+    float minimum = (*data)[i];
+
+    for(i=0; i<n; i++){
+        if((*data)[i] < minimum)
+        minimum = (*data)[i];
+    }
+
+    return minimum;
+}
+
+//call and calculate the getMax() function
+float BigKels::Stattys::getMax(std::vector<float>* data, int n){
+    int i = 0;
+    float maximum = (*data)[i];
+
+    for(i=0; i<n; i++){
+        if((*data)[i] > maximum)
+        maximum = (*data)[i];
+    }
+
+    return maximum;
+}
--- primer2022.cpp
+++ primer2022.cpp
@@ -1,6 +1,15 @@
 #include "primer2022.hpp"
 
+mySpace::yourSpace::myClass::myClass(std::string name, int init_num_students){ //constructors
+    this->className = name;
+    this->num_students = init_num_students;
+}
+
 int mySpace::yourSpace::myClass::get_num_students(){
     //return num_students;
     return this ->num_students;
+}
+
+std::vector<std::string>* mySpace::yourSpace:myClass::get_classlist(){
+    return &(this->classlist);
 }
