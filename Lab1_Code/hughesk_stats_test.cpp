b498ca891775dfc43c2d95b225f74cf1b1df9587
khug410 <kelsey.hughes@marquette.edu>
2022-02-02 17:44:45 -0600

    figuring out printing to the screen and main function

--- Lab1_Code/hughesk_stats_test.cpp
+++ Lab1_Code/hughesk_stats_test.cpp
@@ -81,3 +81,58 @@ float BigKels::Stattys::getSD(std::vector<float>* data, int n, float average){
 
     return STD;
 }
+
+//main function
+int main(int argc, char* argv[]){
+    /*
+    std::vector<float> data;
+    //create a variable to hold the info
+    BigKels::Stattys info;
+
+    std::string file;
+    std::string data1;
+
+    //declare variables
+    float min;
+    float max;
+    float mean;
+    float STD;
+    int n = 0;
+    int i = 0;
+
+    file = argv[1]; //prompted to enter file name
+    std::ifstream list;
+    list.open(file);
+
+    //read the file -- count the number of data point
+    if(list.is_open()){
+        
+        while(std::getline(list, data1)){
+            data.push_back(stof(data1));
+            n++; //prints the number of data pointa
+        }
+    }
+    else{
+        std::cout<<"File cannot be found" << "\n";
+    }
+
+    //write the number of data points
+    std::cout << "# of Data Points: " << n << "\n";
+
+    //call the min, max, mean, and std functions
+    min = info.getMin(&data, n);
+    max = info.getMax(&data, n);
+    mean = info.getMean(&data, n);
+    STD = info.getSD(&data,n,mean);
+
+    //print info
+    std::cout << "Min value is: " << min << "\n";
+    std::cout << "Max value is: " << max << "\n";
+    std::cout << "Mean value is: " << mean << "\n";
+    std::cout << "Standard Deviation value is: " << STD << "\n";
+    */
+}
+
+
+
+
